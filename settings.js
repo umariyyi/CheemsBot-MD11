85c9a39d719be8cbb2fd3b7aa2a01db8f76ba7f5
umariyyi <163399101+umariyyi@users.noreply.github.com>
2024-03-14 10:41:14 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,8 +20,8 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.ownernumber = '256763155715' //ur owner number
+global.ownername = 'Bot deployer' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
